dd478c55757d70faa519294f41eff5abf3a8461d
MarioCake <weberlein.david+github@gmail.com>
2023-12-06 13:18:47 +0100

    Add performance measuring.

--- AOC/Program.cs
+++ AOC/Program.cs
@@ -1,12 +1,20 @@
-﻿using AOC;
+﻿using System.Diagnostics;
+using AOC;
 
 string day1Input = File.ReadAllText("day1.txt");
 string day2Input = File.ReadAllText("day2.txt");
 string day3Input = File.ReadAllText("day3.txt");
 
-Console.WriteLine("Day 1, Part 1: {0}", Day1.Part1(day1Input));
-Console.WriteLine("Day 1, Part 2: {0}", Day1.Part2(day1Input));
-Console.WriteLine("Day 2, Part 1: {0}", Day2.Part1(day2Input));
-Console.WriteLine("Day 2, Part 2: {0}", Day2.Part2(day2Input));
-Console.WriteLine("Day 3, Part 1: {0}", Day3.Part1(day3Input));
-Console.WriteLine("Day 3, Part 2: {0}", Day3.Part2(day3Input));
+Stopwatch stopwatch = Stopwatch.StartNew();
+Console.WriteLine("Day 1, Part 1: {0}, {1}ms", Day1.Part1(day1Input), stopwatch.ElapsedMilliseconds);
+stopwatch.Restart();
+Console.WriteLine("Day 1, Part 2: {0}, {1}ms", Day1.Part2(day1Input), stopwatch.ElapsedMilliseconds);
+stopwatch.Restart();
+Console.WriteLine("Day 2, Part 1: {0}, {1}ms", Day2.Part1(day2Input), stopwatch.ElapsedMilliseconds);
+stopwatch.Restart();
+Console.WriteLine("Day 2, Part 2: {0}, {1}ms", Day2.Part2(day2Input), stopwatch.ElapsedMilliseconds);
+stopwatch.Restart();
+Console.WriteLine("Day 3, Part 1: {0}, {1}ms", Day3.Part1(day3Input), stopwatch.ElapsedMilliseconds);
+stopwatch.Restart();
+Console.WriteLine("Day 3, Part 2: {0}, {1}ms", Day3.Part2(day3Input), stopwatch.ElapsedMilliseconds);
+stopwatch.Restart();
