56967717fa29e07814c3c584fcc59bca13d168c5
Alexander Fritsch <35202909+loudar@users.noreply.github.com>
2023-12-06 14:21:37 +0100

    timing

---
+++ AOC/DayRunner.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace AOC;
+
+public static class DayRunner
+{
+    public static void Run(int times)
+    {
+        RunAndCalculateAverage(Day1.Part1, File.ReadAllText("day1.txt"), times, "Day 1, Part 1");
+        RunAndCalculateAverage(Day1.Part2, File.ReadAllText("day1.txt"), times, "Day 1, Part 2");
+        RunAndCalculateAverage(Day2.Part1, File.ReadAllText("day2.txt"), times, "Day 2, Part 1");
+        RunAndCalculateAverage(Day2.Part2, File.ReadAllText("day2.txt"), times, "Day 2, Part 2");
+        RunAndCalculateAverage(Day3.Part1, File.ReadAllText("day3.txt"), times, "Day 3, Part 1");
+        RunAndCalculateAverage(Day3.Part2, File.ReadAllText("day3.txt"), times, "Day 3, Part 2");
+    }
+
+    private static void RunAndCalculateAverage(Func<string, string> func, string input, int times, string label)
+    {
+        double totalMilliseconds = 0;
+
+        for (int i = 0; i < times; i++)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            string result = func(input);
+            stopwatch.Stop();
+            totalMilliseconds += stopwatch.Elapsed.TotalMilliseconds;
+        }
+
+        double averageMilliseconds = totalMilliseconds / times;
+        Console.WriteLine($"{label}: {averageMilliseconds.ToString("F2")}ms (average over {times} runs)");
+    }
+}
--- AOC/Program.cs
+++ AOC/Program.cs
@@ -1,20 +1,3 @@
-﻿using System.Diagnostics;
-using AOC;
+﻿using AOC;
 
-string day1Input = File.ReadAllText("day1.txt");
-string day2Input = File.ReadAllText("day2.txt");
-string day3Input = File.ReadAllText("day3.txt");
-
-Stopwatch stopwatch = Stopwatch.StartNew();
-Console.WriteLine("Day 1, Part 1: {0}, {1}ms", Day1.Part1(day1Input), stopwatch.ElapsedMilliseconds);
-stopwatch.Restart();
-Console.WriteLine("Day 1, Part 2: {0}, {1}ms", Day1.Part2(day1Input), stopwatch.ElapsedMilliseconds);
-stopwatch.Restart();
-Console.WriteLine("Day 2, Part 1: {0}, {1}ms", Day2.Part1(day2Input), stopwatch.ElapsedMilliseconds);
-stopwatch.Restart();
-Console.WriteLine("Day 2, Part 2: {0}, {1}ms", Day2.Part2(day2Input), stopwatch.ElapsedMilliseconds);
-stopwatch.Restart();
-Console.WriteLine("Day 3, Part 1: {0}, {1}ms", Day3.Part1(day3Input), stopwatch.ElapsedMilliseconds);
-stopwatch.Restart();
-Console.WriteLine("Day 3, Part 2: {0}, {1}ms", Day3.Part2(day3Input), stopwatch.ElapsedMilliseconds);
-stopwatch.Restart();
+DayRunner.Run(10000);
